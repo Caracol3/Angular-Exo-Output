9dd916c327e699d6ee7e8753b91912683117c1a9
Mathieu Coulibaly <mathieu.coulibaly@reseau.sncf.fr>
2023-06-12 10:15:03 +0200

    Quest Output on Odissey achieved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Output';
+
+  OnomatopoeiaList: string[] = [];
+
+  onReceiveOnomatopoeia(event: string): void {
+  this.OnomatopoeiaList.push(event);
+
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { CreateOnomatopoeiaComponent } from './create-onomatopoeia/create-onomatopoeia.component';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    CreateOnomatopoeiaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
